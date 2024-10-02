62495b475d56c98e5f3e4e6224e629d04aefbb5b
Efend1 <kujanglarang31@gmail.com>
2024-10-02 09:56:56 +0700

    update latihan2

---
+++ latihan2/loops.js
@@ -0,0 +1,14 @@
+//for loop
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+for (let x = 10; x >= 0; x--) {
+  console.log(x);
+}
+
+let teks = "JAVASCRIPT";
+for (let j = 0; j < teks.length; j++) {
+  console.log(teks[j]);
+}
